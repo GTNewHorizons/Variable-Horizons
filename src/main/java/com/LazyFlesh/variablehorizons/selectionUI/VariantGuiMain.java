cff7469991245d77a831ceac03fddf5211801403
GDCloud <gdcloudstrike@gmail.com>
2026-08-11 01:02:46 +0200

    it exists

---
+++ src/main/java/com/LazyFlesh/variablehorizons/selectionUI/VariantGuiMain.java
@@ -0,0 +1,73 @@
+package com.LazyFlesh.variablehorizons.selectionUI;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiSelectWorld;
+import net.minecraftforge.client.event.GuiScreenEvent;
+
+import com.gtnewhorizon.gtnhlib.eventbus.EventBusSubscriber;
+
+import cpw.mods.fml.common.eventhandler.EventPriority;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.Side;
+
+@EventBusSubscriber(side = Side.CLIENT)
+public class VariantGuiMain extends GuiScreen {
+
+    private final GuiScreen parent;
+    private static final int OPENING_BUTTON_ID = 8192;
+
+    public VariantGuiMain(GuiScreen parent) {
+        this.parent = parent;
+    }
+
+    @SuppressWarnings("unchecked")
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void onGuiInit(GuiScreenEvent.InitGuiEvent.Post event) {
+        if (event.gui instanceof GuiSelectWorld gui) {
+            event.buttonList
+                .add(new GuiVariantsButton(6, event.gui.height - 52, 82, 20, "Variants", gui));
+        }
+    }
+
+    private static class GuiVariantsButton extends GuiButton {
+
+        private final GuiSelectWorld gui;
+
+        public GuiVariantsButton(int x, int y, int widthIn, int heightIn, String buttonText, GuiSelectWorld gui) {
+            super(OPENING_BUTTON_ID, x, y, widthIn, heightIn, buttonText);
+            this.gui = gui;
+        }
+
+        @Override
+        public boolean mousePressed(Minecraft mc, int mouseX, int mouseY) {
+            if (!super.mousePressed(mc, mouseX, mouseY)) {
+                return false;
+            }
+
+            mc.displayGuiScreen(new VariantGuiMain(gui));
+
+            return true;
+        }
+    }
+
+    @Override
+    public void initGui() {
+        this.buttonList.add(new GuiButton(0, this.width / 2 - 100, this.height - 27, 200, 20, "Done"));
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) {
+        if (button.id == 0) {
+            this.mc.displayGuiScreen(parent);
+        }
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+        this.drawCenteredString(this.fontRendererObj, "Variants", this.width / 2, 20, 0xFFFFFF);
+        super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+}
