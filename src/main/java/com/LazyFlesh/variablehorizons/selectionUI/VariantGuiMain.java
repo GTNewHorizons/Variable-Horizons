b49835ea8d57b22c40ef7a09f975422df46d5f76
GDCloud <gdcloudstrike@gmail.com>
2026-08-12 00:40:17 +0200

    progress

--- src/main/java/com/LazyFlesh/variablehorizons/selectionUI/VariantGuiMain.java
+++ src/main/java/com/LazyFlesh/variablehorizons/selectionUI/VariantGuiMain.java
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiSelectWorld;
 import net.minecraft.client.gui.GuiSlot;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.util.StatCollector;
 import net.minecraftforge.client.event.GuiScreenEvent;
 
 import com.LazyFlesh.variablehorizons.variants.VariantNames;
@@ -23,6 +24,8 @@ public class VariantGuiMain extends GuiScreen {
     private int selectedIndex = -1;
     private final GuiScreen parent;
     private static final int OPENING_BUTTON_ID = 8192;
+    private static final int SIDEBAR_WIDTH = 200;
+    private static final int PADDING = 6;
     private static final List<VariantNames> fullVariants = VariantNames.allCompositionVariants;
     private static final List<VariantNames> subVariants = VariantNames.allSubVariants;
     private VariantList optionList;
@@ -64,21 +67,51 @@ public class VariantGuiMain extends GuiScreen {
     @Override
     public void initGui() {
         this.buttonList.add(new GuiButton(0, this.width / 2 - 100, this.height - 27, 200, 20, "Done"));
+        this.buttonList.add(new GuiButton(1, PADDING, this.height - 52, SIDEBAR_WIDTH - PADDING, 20, "Toggle"));
         this.optionList = new VariantList();
+
+        updateBottomButtons();
+    }
+
+    private void updateBottomButtons() {
+        this.buttonList.get(1).enabled = selectedIndex >= 0 && selectedIndex < fullVariants.size();
     }
 
     @Override
     protected void actionPerformed(GuiButton button) {
         if (button.id == 0) {
             this.mc.displayGuiScreen(parent);
+        } else if (button.id == 1) {
+           // Toggle variant if possible
         }
     }
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         this.optionList.drawScreen(mouseX, mouseY, partialTicks);
+        drawDetailsPanel();
         this.drawCenteredString(this.fontRendererObj, "Variants", this.width / 2, 20, 0xFFFFFF);
         super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+
+    private void drawDetailsPanel() {
+        int panelX = SIDEBAR_WIDTH + PADDING * 2;
+        int panelY = 32;
+
+        if (selectedIndex < 0 || selectedIndex >= fullVariants.size()) {
+            return;
+        }
+
+        String entry = fullVariants.get(selectedIndex).id;
+        this.drawString(this.fontRendererObj, entry, panelX, panelY, 0xFFFFFF);
+
+        int wrapWidth = this.width - panelX - PADDING;
+        List<String> lines = this.fontRendererObj.listFormattedStringToWidth("blablabla", wrapWidth);
+        int lineY = panelY + 20;
+        for (String line : lines) {
+            this.drawString(this.fontRendererObj, line, panelX, lineY, 0xCCCCCC);
+            lineY += this.fontRendererObj.FONT_HEIGHT + 2;
+        }
     }
 
     class VariantList extends GuiSlot {
@@ -86,11 +119,22 @@ public class VariantGuiMain extends GuiScreen {
         VariantList() {
             super(
                 VariantGuiMain.this.mc,
-                VariantGuiMain.this.width,
+                SIDEBAR_WIDTH,
                 VariantGuiMain.this.height,
                 32,
-                VariantGuiMain.this.height - 64,
+                VariantGuiMain.this.height - 60,
                 20);
+            this.setSlotXBoundsFromLeft(0);
+        }
+
+        @Override
+        public int getListWidth() {
+            return SIDEBAR_WIDTH - PADDING;
+        }
+
+        @Override
+        protected int getScrollBarX() {
+            return SIDEBAR_WIDTH - 6;
         }
 
         @Override
@@ -101,6 +145,7 @@ public class VariantGuiMain extends GuiScreen {
         @Override
         protected void elementClicked(int index, boolean doubleClick, int mouseX, int mouseY) {
             selectedIndex = index;
+            updateBottomButtons();
         }
 
         @Override
@@ -116,8 +161,11 @@ public class VariantGuiMain extends GuiScreen {
         @Override
         protected void drawSlot(int index, int x, int y, int slotHeight, Tessellator tessellator, int mouseX,
             int mouseY) {
-            String label = fullVariants.get(index).id;
-            VariantGuiMain.this.drawString(VariantGuiMain.this.fontRendererObj, label, x + 2, y + 6, 0xFFFFFF);
+            String label = StatCollector.translateToLocal("variants." + fullVariants.get(index).id + ".name");
+            int xOffset = x + (getListWidth() - 4) / 2;
+            int yOffset = y + (slotHeight - VariantGuiMain.this.fontRendererObj.FONT_HEIGHT) / 2;
+            VariantGuiMain.this
+                .drawCenteredString(VariantGuiMain.this.fontRendererObj, label, xOffset, yOffset, 0xFFFFFF);
         }
     }
 }
