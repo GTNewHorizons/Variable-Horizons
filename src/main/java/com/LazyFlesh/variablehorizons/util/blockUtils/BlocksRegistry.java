afd9e4f24e02f17d02a637ede9c24b52d965be7b
GDCloud <gdcloudstrike@gmail.com>
2026-09-03 00:48:43 +0200

    expand blacklist to allow more than 1 meta per block

--- src/main/java/com/LazyFlesh/variablehorizons/util/blockUtils/BlocksRegistry.java
+++ src/main/java/com/LazyFlesh/variablehorizons/util/blockUtils/BlocksRegistry.java
@@ -2,9 +2,11 @@ package com.LazyFlesh.variablehorizons.util.blockUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
@@ -19,18 +21,28 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class BlocksRegistry {
 
     private static List<BlockData> blocks;
-    private static Map<String, Integer> BLACKLIST = new HashMap<>() {
-        {
-            put("tectech:Eye of Harmony Renderer", 0);
-            put("tectech:ForgeOfGodsRenderBlock", 0);
-            put("gregtech:gt.nanoforgerenderer", 0);
-            put("gregtech:gt.blackholerenderer", 0);
-            put("gregtech:gt.wormholerenderer", 0);
-            put("GoodGenerator:antimatterRenderBlock", 0);
-            put("HardcoreEnderExpansion:corrupted_energy_high", 0);
-            put("HardcoreEnderExpansion:corrupted_energy_low", 0);
+    private static final Map<String, Set<Integer>> BLACKLIST = new HashMap<>();
+
+    static {
+        // Meta -1 blacklists all metadata variants for a block
+        blacklistBlock("tectech:Eye of Harmony Renderer", 0);
+        blacklistBlock("tectech:ForgeOfGodsRenderBlock", 0);
+        blacklistBlock("gregtech:gt.nanoforgerenderer", 0);
+        blacklistBlock("gregtech:gt.blackholerenderer", 0);
+        blacklistBlock("gregtech:gt.wormholerenderer", 0);
+        blacklistBlock("GoodGenerator:antimatterRenderBlock", 0);
+        blacklistBlock("HardcoreEnderExpansion:corrupted_energy_high", 0);
+        blacklistBlock("HardcoreEnderExpansion:corrupted_energy_low", 0);
+        blacklistBlock("OpenBlocks:tank", 0);
+        blacklistBlock("ExtraUtilities:drum", 0, 1);
+    }
+
+    private static void blacklistBlock(String blockID, int... metas) {
+        Set<Integer> metaSet = BLACKLIST.computeIfAbsent(blockID, k -> new HashSet<>());
+        for (int m : metas) {
+            metaSet.add(m);
         }
-    };
+    }
 
     public static List<BlockData> getBlocks() {
         if (blocks != null) return blocks;
@@ -92,7 +104,8 @@ public class BlocksRegistry {
 
     private static boolean checkIfBlacklisted(String blockID, int meta) {
         if (BLACKLIST.containsKey(blockID)) {
-            return BLACKLIST.get(blockID) == meta;
+            Set<Integer> blacklistedMetas = BLACKLIST.get(blockID);
+            return (blacklistedMetas.contains(meta) || blacklistedMetas.contains(-1));
         }
         return false;
     }
