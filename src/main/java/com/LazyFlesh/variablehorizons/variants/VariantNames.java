c6b88d18d1a922010e19d78a1fa5b7fa8a4693ba
GDCloud <gdcloudstrike@gmail.com>
2026-09-03 01:53:45 +0200

    add incompatibility

--- src/main/java/com/LazyFlesh/variablehorizons/variants/VariantNames.java
+++ src/main/java/com/LazyFlesh/variablehorizons/variants/VariantNames.java
@@ -36,7 +36,7 @@ public enum VariantNames {
     INFINITE_POWER("INFINITE_POWER", new InfinitePower()),
     CUSTOM_DIM_START("CUSTOM_DIM_START", new DiffDimStart()), // sets a different dim as the spawn dimension instead of OW
     SUPERFLAT("SUPERFLAT", new VariantNames[]{ VOID_WORLD, VOID_ISLAND }),
-    MONOBLOCK("MONOBLOCK"),
+    MONOBLOCK("MONOBLOCK", new VariantNames[]{ NO_RECIPE_ADDITIONS }),
 
     // full variants
     // i.e. defines both world type and recipes
