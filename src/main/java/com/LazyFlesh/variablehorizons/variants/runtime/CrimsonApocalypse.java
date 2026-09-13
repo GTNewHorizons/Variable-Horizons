35eefc6356a80d0e7bc01be76defe2105ec79063
GDCloud <gdcloudstrike@gmail.com>
2026-09-14 00:33:17 +0200

    enable all special mob types except thief skeletons

--- src/main/java/com/LazyFlesh/variablehorizons/variants/runtime/CrimsonApocalypse.java
+++ src/main/java/com/LazyFlesh/variablehorizons/variants/runtime/CrimsonApocalypse.java
@@ -1,5 +1,7 @@
 package com.LazyFlesh.variablehorizons.variants.runtime;
 
+import java.lang.reflect.Field;
+
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
@@ -8,18 +10,31 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 
 import com.LazyFlesh.variablehorizons.Config.MobConfig;
+import com.LazyFlesh.variablehorizons.VariableHorizons;
 import com.LazyFlesh.variablehorizons.variants.VariantLoader;
 import com.LazyFlesh.variablehorizons.variants.VariantNames;
 
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import toast.specialMobs.Properties;
+import toast.specialMobs.RandomHelper;
+import toast.specialMobs._SpecialMobs;
 
 public class CrimsonApocalypse extends VariantLoader implements IRuntimeVariant {
 
     @Override
     public void loadVariant(VariantNames... activeVariants) {
         VariantNames.CRIMSON_APOCALYPSE.hasLoaded = true;
+        for (String mobName : _SpecialMobs.MONSTER_KEY) {
+            modifySpecialMobsWeight(mobName, "_vanilla", 1);
+        }
 
+        for (int i = 0; i < _SpecialMobs.MONSTER_TYPES.length; i++) {
+            for (String mobVariant : _SpecialMobs.MONSTER_TYPES[i]) {
+                modifySpecialMobsWeight(_SpecialMobs.MONSTER_KEY[i], mobVariant, 3);
+            }
+        }
+        modifySpecialMobsWeight("Skeleton", "Thief", 0);
     }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
@@ -50,6 +65,57 @@ public class CrimsonApocalypse extends VariantLoader implements IRuntimeVariant 
         }
     }
 
+    private void modifySpecialMobsWeight(String mobName, String targetType, int newWeight) {
+        int mobIndex = -1;
+        int typeIndex = -1;
+
+        for (int i = 0; i < _SpecialMobs.MONSTER_KEY.length; i++) {
+            if (_SpecialMobs.MONSTER_KEY[i].equalsIgnoreCase(mobName)) {
+                mobIndex = i;
+                break;
+            }
+        }
+
+        if (mobIndex == -1) {
+            VariableHorizons.LOG.warn("Could not find Special Mobs base mob: {}", mobName);
+            return;
+        }
+
+        if (targetType.equalsIgnoreCase("_vanilla")) {
+            typeIndex = 0;
+        } else {
+            for (int j = 0; j < _SpecialMobs.MONSTER_TYPES[mobIndex].length; j++) {
+                if (_SpecialMobs.MONSTER_TYPES[mobIndex][j].equalsIgnoreCase(targetType)) {
+                    // +1 because index 0 is taken by _vanilla
+                    typeIndex = j + 1;
+                    break;
+                }
+            }
+        }
+
+        if (typeIndex == -1) {
+            VariableHorizons.LOG.warn("Could not find mob variant: {} for {}", targetType, mobName);
+            return;
+        }
+
+        int clampedNewWeight = Math.max(0, newWeight);
+        int[][] liveWeights = Properties.monsterWeights();
+        int oldWeight = liveWeights[mobIndex][typeIndex];
+        int weightDifference = clampedNewWeight - oldWeight;
+        liveWeights[mobIndex][typeIndex] = clampedNewWeight;
+
+        if (weightDifference != 0) {
+            try {
+                Field totalWeightsField = RandomHelper.class.getDeclaredField("totalMonsterWeights");
+                totalWeightsField.setAccessible(true);
+                int[] totalWeights = (int[]) totalWeightsField.get(null);
+                totalWeights[mobIndex] += weightDifference;
+            } catch (Exception e) {
+                VariableHorizons.LOG.warn("Failed to update total mob weights");
+            }
+        }
+    }
+
     @Override
     public void variantRecipes(VariantNames... activeVariants) {
         // none to add
@@ -58,5 +124,17 @@ public class CrimsonApocalypse extends VariantLoader implements IRuntimeVariant 
     @Override
     public void undoVariant(VariantNames... activeVariants) {
         VariantNames.CRIMSON_APOCALYPSE.hasLoaded = false;
+        for (String mobName : _SpecialMobs.MONSTER_KEY) {
+            modifySpecialMobsWeight(mobName, "_vanilla", 3);
+        }
+
+        for (int i = 0; i < _SpecialMobs.MONSTER_TYPES.length; i++) {
+            for (String mobVariant : _SpecialMobs.MONSTER_TYPES[i]) {
+                modifySpecialMobsWeight(_SpecialMobs.MONSTER_KEY[i], mobVariant, 3);
+            }
+        }
+        modifySpecialMobsWeight("Skeleton", "Thief", 0);
+        modifySpecialMobsWeight("Enderman", "Thief", 0);
+        modifySpecialMobsWeight("Witch", "Wind", 0);
     }
 }
