8d378189b8669c8f84b73a13c89c09bd11e256aa
fehling135 <3552245891@qq.com>
2026-09-17 17:27:31 +0800

    forgot this

--- src/main/java/com/LazyFlesh/variablehorizons/variants/invasive/GardenOfGrind.java
+++ src/main/java/com/LazyFlesh/variablehorizons/variants/invasive/GardenOfGrind.java
@@ -10,6 +10,7 @@ import gregtech.api.enums.GTValues;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.enums.TierEU;
+import gregtech.api.util.GTModHandler;
 import gregtech.api.util.GTOreDictUnificator;
 import gregtech.common.config.Worldgen;
 
